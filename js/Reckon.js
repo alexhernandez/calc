0c74c50435980cc581e7189ecff8845388941461
Alex Hernandez <oorangecchicken@gmail.com>
2016-01-21 23:09:48 -0800

    Reckon UI Integration

---
+++ js/Reckon.js
@@ -0,0 +1,35 @@
+// /*
+// * Reckon v1.0.0
+// * Copyright 2016, Alex Hernandez
+// * https://github.com/alexhernandez/reckon
+// *
+// * Free to use under the MIT license.
+// */
+
+'use strict';
+
+function Reckon () {
+  this.total = 0;
+}
+
+Reckon.prototype.add = function(value) {
+  return this.total += value;
+};
+
+Reckon.prototype.subtract = function(value) {
+  return this.total -= value;
+};
+
+Reckon.prototype.multiply = function(value) {
+  return this.total *= value;
+};
+
+Reckon.prototype.divide = function(value) {
+  return this.total /= value;
+};
+
+Reckon.prototype.clear = function() {
+  return this.total = 0;
+};
+
+module.exports = Reckon;
---
+++ js/ReckonInterface.js
@@ -0,0 +1,13 @@
+// /*
+// * Reckon v1.0.0
+// * Copyright 2016, Alex Hernandez
+// * https://github.com/alexhernandez/reckon
+// *
+// * Free to use under the MIT license.
+// */
+
+function ReckonInterface () {
+
+}
+
+module.exports = ReckonInterface;
